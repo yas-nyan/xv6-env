de11d0e96b45586a2a00eaa95044a7db3b0e7205
yas-nyan <yas-nyan@sfc.wide.ad.jp>
2021-05-31 16:46:21 +0900

    add scripts

--- exec.sh
+++ exec.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 docker start xv6-ubuntu
-docker exec -it `docker ps | grep xv6-ubuntu | awk '{print $1}'` bash
+docker exec -it `docker ps | grep xv6-ubuntu | awk '{print $1}'` 'bash'
---
+++ stop.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker stop xv6-ubuntu
